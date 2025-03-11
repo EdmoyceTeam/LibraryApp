fb491829a46432d45343e44c69dfccb6c70336cd
EmmaAibu <emmaaibu40@gmail.com>
2025-03-11 11:53:52 +0000

    add hero section

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,10 +1,29 @@
 import React from "react";
 import PagesLayouts from "../layouts/PagesLayouts";
+import Logo from "../assets/images/Logo.png";
 
 const Landing = () => {
   return (
     <PagesLayouts>
-      <div>Landing</div>
+      <div>
+        <h1>Edmoyce</h1>
+        <p>Library</p>
+        <div>
+          <p>
+            "Your Premier Digital Library Management for updating book resource"
+          </p>
+        </div>
+      </div>
+      <div>
+        <h2>Welcome Back !!</h2>
+        <p>Please Enter Your Credntials To Log in</p>
+        <div>
+          <input type="text" placeholder="Username" />
+          <input type="text" placeholder="Password" />
+          <button>Forgot Password</button>
+        </div>
+        <button>SIGN IN</button>
+      </div>
     </PagesLayouts>
   );
 };
